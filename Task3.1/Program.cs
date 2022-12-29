914b72a30b4948706847646cb3b4d45240178fd5
irfan <irfanfy@code.edu.az.com>
2022-12-29 14:32:10 +0400

    task3.1

---
+++ Task3.1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine()); 
+            int count = (int)Math.Log10(number) + 1;
+            if (count == 5)
+            {
+                int result = (400000 + number) * 10 + 2;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("5 reqemli olalidir");
+            }
+                
+
+        }
+    }
+}
